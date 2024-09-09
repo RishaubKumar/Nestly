7bd2ef042007c19ad029fa1802b3aaafe4ed7d67
Rishaub Kumar <rishaubkumar534@gmail.com>
2024-09-10 00:09:34 +0530

    review added

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js")
+const {listingSchema,reviewSchema} = require("./schema.js")
+const Review = require("./models/review.js");
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Nestly";
 
@@ -34,6 +36,15 @@ app.get("/", (req, res) => {
 });
 const validatelisting = (req,res,next)=>{
   let {error} =  listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map((el)=>el.message).join(",");
+   throw new ExpressError(400,errMsg);
+ }else{
+  next();
+ }
+}
+const validateReview = (req,res,next)=>{
+  let {error} =  reviewSchema.validate(req.body);
   if(error){
     let errMsg = error.details.map((el)=>el.message).join(",");
    throw new ExpressError(400,errMsg);
@@ -91,6 +102,19 @@ app.delete("/listings/:id",wrapAsync(async (req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }));
+
+//Review Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+      let listing = await Listing.findById(req.params.id);
+      let newReview = new Review(req.body.review);
+      listing.reviews.push(newReview); 
+      await newReview.save();
+      await listing.save();
+      console.log("New review saved");
+      res.send("New review saved");
+  
+}));
+
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
