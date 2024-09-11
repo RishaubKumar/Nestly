e33ec4ecb8cda682304c3e0d5177f795ce68742a
Rishaub Kumar <rishaubkumar534@gmail.com>
2024-09-11 23:35:46 +0530

    Revert "routes added"
    This reverts commit 033bae5a736526c64b30f86629df7a62b7c80e7a.

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review = require("./review.js");
-
 
 const listingSchema = new Schema({
   title: {
@@ -27,11 +25,5 @@ const listingSchema = new Schema({
   }]
 });
 
-listingSchema.post("findOneAndDelete",async()=>{
-  if(listing){
-    await Review.deleteMany({_id:{$in: listing.reviews}});
-
-  }
-})
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
