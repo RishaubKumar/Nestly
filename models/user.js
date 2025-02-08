7c0ad698e0e1daeab01569b03b6771278de579a0
Rishaub Kumar <rishaubkumar534@gmail.com>
2025-02-08 23:19:42 +0530

    listing schema corrected

--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
-    fullName: { type: String, required: true },  // This field should exist
+    // fullName: { type: String, required: true },  // This field should exist
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    // password: { type: String, required: true }
   });
   
 userSchema.plugin(passportLocalMongoose);
